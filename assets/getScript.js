2c77bae7184f0ca8d256028e72297dd905ac0cc4
gna-rus <gnarus@inbox.ru>
2023-06-19 16:48:18 +0300

    подключил скрипт для подачи запросов  и вывожу на экран инфромацию загружаемая с другого сайта(getScript.js)

--- assets/getScript.js
+++ assets/getScript.js
@@ -4,6 +4,16 @@ async function start(){
     let response = await fetch(url);
     // сохраняем ответ с сервера в формате json
     let users = await response.json();
+    // создаю пустую еременную
+    let html1 = '';
+    // перебор массива
+    users.forEach(elemnt => {
+    let html1Segment = `<h2>${elemnt.name}</h2>`
+    html1 += html1Segment});
     console.log(users);
+
+    // получаем нужную секцию и загружаю ее в поле на сайте
+    let section = document.getElementById("allUsers");
+    section.innerHTML = html1; // передаю накопленную строку в секцию
 }
 start();
